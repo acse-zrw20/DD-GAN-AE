b78268be3ded634fea645c540eec43f7cc78fdae
Zeff020 <zefwolffs@gmail.com>
2021-07-05 13:31:51 +0100

    hpc file edit

--- hpc/train_wandb_sf.sh
+++ hpc/train_wandb_sf.sh
@@ -2,7 +2,7 @@
 #PBS -N wandb_run_sf
 #PBS -o wandb_run_sf.out
 #PBS -j oe
-#PBS -lselect=1:ncpus=4:mem=24gb:ngpus=1
+#PBS -lselect=1:ncpus=4:mem=24gb:ngpus=4:gpu_type=RTX6000
 #PBS -lwalltime=24:00:00
 set -vx
 
@@ -12,7 +12,7 @@ module load anaconda3/personal
 module load cuda/11.0.1
 source activate tf 
 nvidia-smi
-python train_wandb_sf.py --model="$0" --datafile="$HOME/data/processed/sf_snapshots_200timesteps_rand.npy"
+python train_wandb_sf.py --model="$model" --datafile="$HOME/data/processed/sf_snapshots_200timesteps_rand.npy"
 
 mkdir $WORK/$PBS_JOBID
 cp * $WORK/$PBS_JOBID
