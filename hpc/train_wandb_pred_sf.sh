b6214b5befd8c16da74055f36ae21d430a12accc
Zeff020 <zefwolffs@gmail.com>
2021-07-21 18:47:21 +0100

    Added hpc script for hyperparameter optimization

---
+++ hpc/train_wandb_pred_sf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -N wandb_run_pred_sf
+#PBS -o wandb_run_pred_sf.out
+#PBS -j oe
+#PBS -lselect=1:ncpus=4:mem=24gb:ngpus=1:gpu_type=RTX6000
+#PBS -lwalltime=24:00:00
+set -vx
+
+# Need to set the weights and biases API key
+export WANDB_API_KEY=$api_key
+
+module purge
+module load anaconda3/personal
+module load cuda/11.0.1
+source activate tf
+
+# Always install newest version of local package
+cd /rds/general/user/zrw20/home/DD-GAN-AE
+pip install -e .
+
+cd /rds/general/user/zrw20/home/DD-GAN-AE/ddganAE/wandb
+nvidia-smi
+python train_wandb_sf.py --datafile="$HOME/data/processed/cae_latent_sf_10vars_800steps_different.npy" --niters=200 --savemodel="False"
+
+mkdir $WORK/$PBS_JOBID
+cp * $WORK/$PBS_JOBID
+
+echo "... Run finished $(date) ..."
