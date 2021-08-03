9c0ceb99566fdb3f6caa46b422ce1343f11fc70e
Zeff020 <zefwolffs@gmail.com>
2021-08-03 14:52:51 +0100

    Added predictive ae

--- ddganAE/models/predictive.py
+++ ddganAE/models/predictive.py
@@ -16,7 +16,7 @@ import wandb
 
 class Predictive_adversarial:
     """
-    Predictive adversarial SVD Autoencoder class
+    Predictive adversarial Autoencoder class
     """
 
     def __init__(self, encoder, decoder, discriminator, optimizer):
@@ -357,5 +357,291 @@ class Predictive_adversarial:
                         pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
                             (self.adversarial_autoencoder.predict(
                                 pred_vars_t)[0][0] - pred_vars[k, :, i]) * sor
+
+        return pred_vars
+
+
+class Predictive:
+    """
+    Predictive Autoencoder class
+    """
+
+    def __init__(self, encoder, decoder, optimizer):
+        self.encoder = encoder
+        self.decoder = decoder
+        self.latent_dim = self.decoder.layers[0].input_shape[1]
+
+        self.optimizer = optimizer
+
+    def compile(self, nPOD, increment=False):
+        """
+        Compile model
+
+        Args:
+            input_shape (tuple): Shape of input data
+        """
+
+        self.increment = increment
+        self.nPOD = nPOD
+        if isinstance(self.encoder.layers[0], Conv1D):
+            # Convolutional networks require a slightly different input shape
+            self.input_shape = (1, 3*nPOD)
+        else:
+            self.input_shape = (3*nPOD,)
+
+        grid = Input(shape=self.input_shape)
+        encoded_repr = self.encoder(grid)
+        gen_grid = self.decoder(encoded_repr)
+        self.autoencoder = Model(grid, gen_grid)
+
+        self.autoencoder.compile(optimizer=self.optimizer,
+                                 loss="mse",
+                                 metrics=['accuracy'])
+
+    def preprocess(self, input_data):
+
+        # Preprocessing
+        for k in range(self.interval):
+            grid_coeffs = np.array(input_data)[:, :, k::self.interval]
+            train_data = np.zeros((grid_coeffs.shape[0] - 2,
+                                   grid_coeffs.shape[1]
+                                   * 3, grid_coeffs.shape[2]))
+
+            for i in range(1, grid_coeffs.shape[0]-1):
+                for j in range(3):
+                    train_data[i-1, j*self.nPOD:(j+1)*self.nPOD, :] = \
+                        grid_coeffs[i+j-1, :, :]
+
+            train_data_swap = train_data.swapaxes(1, 2)
+
+            step = train_data_swap[:, :-1, :]
+            step_forward = train_data_swap[:, 1:, :]
+
+            step[:, :, :self.nPOD] = step_forward[:, :, :self.nPOD]
+            step[:, :, self.nPOD*2:] = step_forward[:, :, self.nPOD*2:]
+
+            x_train = step
+
+            if self.increment:
+                # For if we use the time increment approach
+                y_train = step
+                y_train = np.diff(y_train, axis=1)
+                x_train = x_train[:, :-1, :]
+            else:
+                y_train = step_forward
+
+            x_train = np.concatenate(x_train, 0)
+            y_train = np.concatenate(y_train, 0)
+
+            # We only predict the central POD coefficients
+            y_train = y_train[:, self.nPOD:self.nPOD*2]
+
+            if k == 0:
+                x_train_full = x_train
+                y_train_full = y_train
+            else:
+                x_train_full = np.concatenate([x_train_full, x_train])
+                y_train_full = np.concatenate([y_train_full, y_train])
+
+        return x_train_full, y_train_full
+
+    def train(self, input_data, epochs, interval=5, val_size=0,
+              batch_size=128, val_batch_size=128, wandb_log=False,
+              n_discriminator=5, n_gradient_ascent=np.inf, noise_std=0):
+        """
+        Training model where we use a training method that weights
+        the losses of the discriminator and autoencoder and as such combines
+        them into one loss and trains on them simultaneously. Takes in POD
+        coefficients or latent variables. The timestep shifts will be done in
+        this function.
+
+        Args:
+            train_data (np.ndarray): Array with train data, in shape
+                                     (ngrids, nvars, ntime) (rescaled)
+            epochs (int): Number of epochs
+            interval (int): choose every `interval` variables to model
+            val_data (np.ndarray, optional): Array with validation data.
+                Defaults to None.
+            batch_size (int, optional): Batch size. Defaults to 128.
+            plot_losses (bool, optional): Whether to plot losses.
+                Defaults to False.
+            print_losses (bool, optional): Whether to print losses.
+                Defaults to False.
+        """
+
+        val_dataset = None
+        self.interval = interval
+
+        x_full, y_full = self.preprocess(input_data)
+
+        if val_size > 0:
+            x_train, x_val, y_train, y_val = train_test_split(
+                x_full, y_full, test_size=val_size)
+
+        train_dataset = tf.data.Dataset.from_tensor_slices((x_train,
+                                                            y_train))
+        train_dataset = train_dataset.\
+            shuffle(buffer_size=x_train.shape[0],
+                    reshuffle_each_iteration=True).\
+            batch(batch_size,
+                  drop_remainder=True)
+
+        if noise_std > 0:
+            add_noise = tf.keras.Sequential([GaussianNoise(noise_std)])
+            train_dataset = train_dataset.map(lambda x, y:
+                                              (add_noise(float(x),
+                                                         training=True), y))
+        if val_size > 0:
+            val_dataset = tf.data.Dataset.from_tensor_slices((x_val,
+                                                              y_val))
+            val_dataset = val_dataset. \
+                shuffle(buffer_size=x_val.shape[0],
+                        reshuffle_each_iteration=True).\
+                batch(val_batch_size,
+                      drop_remainder=True)
+
+        # Set up tensorboard logging
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        train_log_dir = 'logs/' + current_time + '/train'
+        val_log_dir = 'logs/' + current_time + '/val'
+        train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+        val_summary_writer = tf.summary.create_file_writer(val_log_dir)
+
+        for epoch in range(epochs):
+            loss_cum = 0
+            acc_cum = 0
+            for step, (x, y) in enumerate(train_dataset):
+
+                # Train the autoencoder reconstruction
+                loss, acc = self.autoencoder.train_on_batch(x, y)
+                loss_cum += loss
+                acc_cum += acc
+
+            # Average the loss and accuracy over the entire dataset
+            loss = loss_cum/(step+1)
+            acc = acc_cum/step
+
+            with train_summary_writer.as_default():
+                tf.summary.scalar('loss', loss, step=epoch)
+                tf.summary.scalar('accuracy', acc, step=epoch)
+
+            # Calculate the accuracies on the validation set
+            if val_dataset is not None:
+                loss_val, acc_val = self.validate(val_dataset, val_batch_size)
+
+                with val_summary_writer.as_default():
+                    tf.summary.scalar('loss', loss_val, step=epoch)
+                    tf.summary.scalar('accuracy', acc_val, step=epoch)
+
+            if wandb_log:
+                if val_dataset is not None:
+                    log = {"epoch": epoch, "train_loss": loss,
+                           "train_accuracy": acc,
+                           "valid_loss": loss_val,
+                           "valid_accuracy": acc_val}
+                else:
+                    log = {"epoch": epoch, "train_loss": loss,
+                           "train_accuracy": acc}
+
+                wandb.log(log)
+
+    def validate(self, val_dataset, val_batch_size):
+        """
+        Validate model on previously unseen dataset.
+
+        Args:
+            val_dataset (np.array): Validation dataset
+            val_batch_size (int, optional): Validation batch size. Defaults to
+                                            128.
+
+        Returns:
+            tuple: Validation losses and accuracies
+        """
+
+        loss_cum = 0
+        acc_cum = 0
+        step = 0
+        for step, (x, y) in enumerate(val_dataset):
+
+            # Train the autoencoder reconstruction
+            loss, acc = self.autoencoder.evaluate(x, y,
+                                                  verbose=0)
+            loss_cum += loss
+            acc_cum += acc
+
+        # Average the loss and accuracy over the entire dataset
+        loss = loss_cum/(step+1)
+        acc = acc_cum/(step+1)
+
+        return loss, acc
+
+    def predict(self, boundaries, init_values, timesteps, iters=5, sor=1):
+        """
+        Predict in time using boundaries and initial values for a certain
+        number of timesteps. The timestep shifts will be done in this function
+
+        Args:
+            boundaries (np.ndarray): Boundaries in shape
+                                     (nboundaries (2), nvars, ntimesteps)
+            init_values (np.ndarray): Initial values in shape (ngrids, nvars)
+            timesteps (int): Number of timesteps to predict
+            iters (int): Number of iterations to do before a prediction
+        """
+        boundaries = boundaries[:, :, ::self.interval]
+
+        pred_vars = np.zeros((2 + init_values.shape[0], boundaries.shape[1],
+                              boundaries.shape[2]))
+        pred_vars[0] = boundaries[0]
+        pred_vars[1:-1, :, 0] = init_values
+        pred_vars[-1] = boundaries[1]
+
+        for i in range(timesteps):
+            # Outer "timesteps" loop
+            
+            # Let's start with a linear extrapolation for the predictions
+            if i > 1:
+                for k in range(1, init_values.shape[0]+1):
+                    pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
+                                           (pred_vars[k, :, i] -
+                                            pred_vars[k, :, i-1])
+
+            for j in range(iters):
+                # Inner optimization loop within a timestep
+                for k in range(1, init_values.shape[0]+1):
+
+                    # Loop over the columns that are meant to be predicted
+                    # pred_vars[k, :, i+1] = \
+                    #    self.adversarial_autoencoder.predict(
+                    #        pred_vars[k-1:k+2, :, i].reshape(1, -1))[0]
+
+                    pred_vars_t = \
+                        np.concatenate((pred_vars[k-1, :, i+1],
+                                        pred_vars[k, :, i],
+                                        pred_vars[k+1, :, i+1])).reshape(1, -1)
+
+                    if self.increment:
+                        pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
+                            self.autoencoder.predict(
+                                pred_vars_t)
+                    else:
+                        pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
+                            (self.autoencoder.predict(
+                                pred_vars_t)[0] - pred_vars[k, :, i]) * sor
+
+                for k in range(init_values.shape[0], 0, -1):
+                    # Loop over the columns that are meant to be predicted
+                    pred_vars_t = \
+                        np.concatenate((pred_vars[k-1, :, i+1],
+                                        pred_vars[k, :, i],
+                                        pred_vars[k+1, :, i+1])).reshape(1, -1)
+
+                    if self.increment:
+                        pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
+                            self.autoencoder.predict(
+                                pred_vars_t)
+                    else:
+                        pred_vars[k, :, i+1] = pred_vars[k, :, i] + \
+                            (self.autoencoder.predict(
+                                pred_vars_t)[0] - pred_vars[k, :, i]) * sor
 
         return pred_vars
--- ddganAE/wandb/train_wandb_pred.py
+++ ddganAE/wandb/train_wandb_pred.py
@@ -12,7 +12,7 @@ import os
 import json
 import keras
 from sklearn.preprocessing import MinMaxScaler
-from ddganAE.models import Predictive_adversarial
+from ddganAE.models import Predictive_adversarial, Predictive
 from ddganAE.architectures.svdae import (
     build_vinicius_encoder_decoder,
     build_slimmer_vinicius_encoder_decoder,
@@ -33,7 +33,6 @@ from ddganAE.architectures.discriminators import (
 from ddganAE.wandb.get_snapshots_3d_continuous import \
      get_snapshots_3D
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 __author__ = "Zef Wolffs"
 __credits__ = []
@@ -222,6 +221,207 @@ def train_wandb_pred_aae(config=None):
             noise_std=config.noise_std,
             n_discriminator=config.n_discriminator,
             n_gradient_ascent=config.n_gradient_ascent
+        )
+
+        # Check how well the model actually performs to also predict the
+        # results
+
+        # Create boundaries and initial values arrays for prediction later
+        boundaries = np.zeros((2, config.in_vars, nfiles))
+        boundaries[0] = train_data[4]
+        boundaries[1] = train_data[7]
+
+        init_values = np.zeros((2, config.in_vars))
+        init_values[0] = train_data[5][:, 0]
+        init_values[1] = train_data[6][:, 0]
+
+        predicted = pred_adv.predict(boundaries, init_values,
+                                     int(nfiles/config.interval)-1, iters=5)
+        train_data_int = train_data[4:8, :, ::config.interval]
+
+        mse = tf.keras.losses.MeanSquaredError()
+        mse_pred = mse(predicted[:, :, :int(nfiles/config.interval)-2],
+                       train_data_int[:, :, :int(nfiles/config.interval)-2])\
+            .numpy()
+
+        log = {"prediction_mse": mse_pred}
+
+        wandb.log(log)
+
+        if config.savemodel:
+            dirname = "model_" + wandb.run.name
+            os.mkdir(dirname)
+            pred_adv.encoder.save(dirname + '/encoder')
+            pred_adv.decoder.save(dirname + '/decoder')
+
+
+def train_wandb_pred_ae(config=None):
+    """
+    Construct and subsequently train the model while reporting losses to
+    weights and biases platform. Weights and biases also controls
+    hyperparameters.
+
+    Args:
+        config (dict, optional): Dictionary with hyperparameters, set by
+                                 weights and biases. Defaults to None.
+    """
+    with wandb.init(config=config, tags=["central_doms_pred_mse"]):
+        # If called by wandb.agent, as below,
+        # this config will be set by Sweep Controller
+        config = wandb.config
+
+        # Data processing
+        latent_vars = np.load(config.datafile)
+
+        nfiles = int(latent_vars.shape[0]/config.domains)
+
+        latent_vars_reshaped = np.moveaxis(
+            latent_vars.reshape(nfiles, config.domains, config.in_vars),
+            0, 2)
+
+        train_data = latent_vars_reshaped[:config.domains]
+
+        # Scaling the latent variables
+        scaler = MinMaxScaler((-1, 1))
+        train_data = scaler.fit_transform(
+            train_data.reshape(-1, 1)).reshape(train_data.shape)
+
+        initializer = tf.keras.initializers.RandomNormal(
+            mean=0.0, stddev=0.05, seed=None
+        )
+        if config.optimizer == "nadam":
+            optimizer = tf.keras.optimizers.Nadam(
+                lr=config.learning_rate,
+                beta_1=config.momentum,
+                beta_2=config.beta_2,
+            )
+        elif config.optimizer == "adam":
+            optimizer = tf.keras.optimizers.Adam(
+                lr=config.learning_rate,
+                beta_1=config.momentum,
+                beta_2=config.beta_2,
+            )
+        elif config.optimizer == "sgd":
+            optimizer = tf.keras.optimizers.SGD(
+                learning_rate=config.learning_rate, momentum=config.momentum
+            )
+
+        if config.architecture == "dense":
+            encoder = build_dense_encoder(
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout,
+            )
+            decoder = build_dense_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout,
+                final_act=config.final_act
+            )
+        elif config.architecture == "deeper_dense":
+            encoder = build_deeper_dense_encoder(
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout
+            )
+            decoder = build_deeper_dense_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout,
+                final_act=config.final_act
+            )
+        elif config.architecture == "wider_dense":
+            encoder = build_wider_dense_encoder(
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout
+            )
+            decoder = build_wider_dense_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout,
+                final_act=config.final_act
+            )
+        elif config.architecture == "slimmer_dense":
+            encoder = build_slimmer_dense_encoder(
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout
+            )
+            decoder = build_slimmer_dense_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                info=False,
+                act=config.activation,
+                dropout=config.dropout,
+                final_act=config.final_act
+            )
+        elif config.architecture == "vinicius":
+            encoder, decoder = build_vinicius_encoder_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                act=config.activation,
+                dense_act=config.dense_activation,
+                dropout=config.dropout,
+                reg=config.regularization,
+                batchnorm=config.batch_normalization,
+                final_act=config.final_act
+            )
+        elif config.architecture == "smaller_vinicius":
+            encoder, decoder = build_smaller_vinicius_encoder_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                act=config.activation,
+                dense_act=config.dense_activation,
+                dropout=config.dropout,
+                reg=config.regularization,
+                batchnorm=config.batch_normalization,
+                final_act=config.final_act
+            )
+        elif config.architecture == "slimmer_vinicius":
+            encoder, decoder = build_slimmer_vinicius_encoder_decoder(
+                config.in_vars,
+                config.latent_vars,
+                initializer,
+                act=config.activation,
+                dense_act=config.dense_activation,
+                dropout=config.dropout,
+                reg=config.regularization,
+                batchnorm=config.batch_normalization,
+                final_act=config.final_act
+            )
+
+        pred_adv = Predictive(encoder, decoder,
+                              optimizer)
+        pred_adv.compile(config.in_vars, increment=config.increment)
+        pred_adv.train(
+            train_data,
+            config.epochs,
+            interval=config.interval,
+            batch_size=config.batch_size,
+            val_size=0.1,
+            wandb_log=True,
+            noise_std=config.noise_std
         )
 
         # Check how well the model actually performs to also predict the
@@ -546,6 +746,50 @@ Predictive_adversarial_sweep_config = {
 }
 
 # Configuration options for hyperparameter optimization
+Predictive_ae_sweep_config = {
+    "method": "bayes",
+    "metric": {"name": "prediction_mse", "goal": "minimize"},
+    "parameters": {
+        "architecture": {
+            "values": [
+                "dense",
+                "deeper_dense",
+                "wider_dense",
+                "slimmer_dense",
+                "vinicius",
+                "smaller_vinicius",
+                "slimmer_vinicius",
+            ]
+        },
+        "activation": {"values": ["relu", "elu", "sigmoid", "tanh"]},
+        "in_vars": {"values": [100]},
+        "dense_activation": {"values": ["relu", "linear"]},
+        "batch_size": {"values": [32, 64, 128]},
+        "learning_rate": {"values": [5e-3, 5e-4, 5e-5]},
+        "dropout": {"values": [0.3, 0.55, 0.8]},
+        "optimizer": {"values": ["nadam", "adam", "sgd"]},
+        "momentum": {"values": [0.8, 0.9, 0.98]},
+        "beta_2": {"values": [0.9, 0.999, 0.99999]},
+        "batch_normalization": {"values": [True, False]},
+        "regularization": {"values": [1e-3, 1e-4, 1e-5, 1e-6, 0]},
+        "savemodel": {"values": [False]},
+        "latent_vars": {"values": [60, 100, 300]},
+        "interval": {"values": [1, 2, 4, 6]},
+        "final_act": {
+            "values": [
+              "linear",
+              "sigmoid",
+              "tanh"
+            ]
+        },
+        "noise_std": {"values": [0.00001, 0.001, 0.01, 0.05, 0.1]},
+        "increment": {"values": [True, False]},
+        "epochs": {"values": [100, 200, 500, 1000]},
+        "domains": {"values": [10]}
+    },
+}
+
+# Configuration options for hyperparameter optimization
 Continuous_predictive_adversarial_sweep_config = {
     "method": "random",
     "metric": {"name": "prediction_mse", "goal": "minimize"},
@@ -617,6 +861,10 @@ functionality')
                         default=None,
                         help='folder with autoencoder for generating latent \
 variables')
+    parser.add_argument('--model', type=str, nargs='?',
+                        default=None,
+                        help='Choose either ae (normal autoencoder) or aae \
+(adversarial autoencoder)')
     args = parser.parse_args()
 
     arg_dict = vars(args)
@@ -643,7 +891,23 @@ variables')
                                project='pred-aae', entity='zeff020')
         wandb.agent(sweep_id, continuous_train_wandb_pred_aae, 
                     count=arg_dict['niters'])
-    else:
+    if args.model == "ae":
+        # Use the normal autoencoder for predictions
+        if arg_dict['custom_config'] is not None:
+            with open(arg_dict["custom_config"]) as json_file:
+                Predictive_ae_sweep_config = json.load(json_file)
+        if arg_dict["savemodel"] == "True":
+            Predictive_ae_sweep_config['parameters']['savemodel'] = \
+                {'values': [True]}
+
+        Predictive_ae_sweep_config['parameters']['datafile'] = \
+            {'values': [arg_dict['datafile']]}
+
+        sweep_id = wandb.sweep(Predictive_ae_sweep_config,
+                               project='pred-aae', entity='zeff020')
+        wandb.agent(sweep_id, train_wandb_pred_ae, count=arg_dict['niters'])
+    elif args.model == "aae":
+        # Use the adversarial autoencoder for predictions
         if arg_dict['custom_config'] is not None:
             with open(arg_dict["custom_config"]) as json_file:
                 Predictive_adversarial_sweep_config = json.load(json_file)
