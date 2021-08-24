6774201d8f13ca8f48d007c7b48d00911be04c6a
Zeff020 <zefwolffs@gmail.com>
2021-08-24 18:09:52 +0100

    update tests

--- preprocessing/src/get_pod_coeffs.py
+++ preprocessing/src/get_pod_coeffs.py
@@ -9,11 +9,18 @@ not be used used very often or in a critical/production setting. Therefore
 sustainability  may be lacking.
 """
 
-import u2r  # noqa
 import vtktools
 import numpy as np
 from utils import get_grid_end_points
 import argparse
+import sys
+
+# Backwards compatibility with python 2
+if sys.version_info[0] < 3:
+    import u2r  # noqa
+else:
+    import u2rpy3  # noqa
+    u2r = u2rpy3
 
 __author__ = " Claire Heaney, Zef Wolffs"
 __credits__ = ["Jon Atli Tomasson"]
